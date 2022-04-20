6fd5addf5ed174c780a722c136ebe71e108b25f0
dantef12 <100299384+dantef12@users.noreply.github.com>
2022-04-20 11:16:47 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index <= 9; index++) {
+        product = index + 1 + product
+    }
+    basic.showNumber(product)
+})
+let product = 0
+product = 0
+basic.forever(function () {
+	
+})
